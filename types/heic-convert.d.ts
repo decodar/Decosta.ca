0795050cf42e03b575654b793bbbdfd3cfa0f7a6
decodar <77027799+decodar@users.noreply.github.com>
2026-02-22 15:26:12 -0800

    Add local TypeScript types for heic-convert

---
+++ types/heic-convert.d.ts
@@ -0,0 +1,9 @@
+declare module "heic-convert" {
+  type HeicConvertOptions = {
+    buffer: Buffer;
+    format: "JPEG" | "PNG";
+    quality?: number;
+  };
+
+  export default function heicConvert(options: HeicConvertOptions): Promise<Buffer | ArrayBuffer | Uint8Array>;
+}
